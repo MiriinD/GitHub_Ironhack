1955cd510b8e13dc52c9adaaa0a28e03acc53ce0
MiriinD <Miirigoch@gmail.com>
2024-01-29 18:31:53 +0100

    update

--- Week 3 SQL/LAB Temporary tables, views and CTEs.sql
+++ Week 3 SQL/LAB Temporary tables, views and CTEs.sql
@@ -54,10 +54,91 @@ SELECT first_name, last_name, email, total_rentals, total_paid/total_rentals AS 
 FROM cte_report;
 
 
+#Hard excercise ASSESSMENT SQL
+
+CREATE VIEW members_approved_for_vaucher AS
+SELECT m.member_id, m.email, m.name, SUM(p.price) AS total_spending
+FROM departments AS d
+INNER JOIN sales AS s
+ON d.id= s.department_id
+INNER JOIN products as p
+ON p.id = s.product_id
+INNER JOIN members AS m
+ON m.id= s.member_id 
+WHERE department_id IN (SELECT s.department_id
+						FROM sales AS s
+                        INNER JOIN products AS p
+                        ON s.product_id= p.id
+                        GROUP BY s.department_id
+                        HAVING SUM (p.price) > 10000)
 
 
+GROUP BY member_id, m.email, m.name
+HAVING total_spending >1000;
+                        
+SELECT * FROM members_approved_for_vaucher;
+
+#Try with CTE and call (The logic seems easier, first look for the department, and later the members on that department
+
+WITH departments_10k AS(
+						SELECT s.department_id
+						FROM sales AS s
+                        INNER JOIN products as p
+                        ON s.product_id = p.id
+                        GROUP BY s.department_id
+                        HAVING SUM(p.price) > 10000)
+
+SELECT m.member_id, m.email, m.name, SUM(p.price) AS total_spending
+FROM departments AS d
+INNER JOIN sales AS s
+ON d.id= s.department_id
+INNER JOIN products as p
+ON p.id = s.product_id
+INNER JOIN members AS m
+ON m.id= s.member_id 
+WHERE department_id IN( SELECT  department_id
+						FROM departments_10k)
+GROUP BY member_id, m.email, m.name
+HAVING tot_spending > 1000;
 
 
+#ASSESSMENT WRITTEN BY ALEX.
+CREATE VIEW members_voucher AS
+SELECT m.member_id, m.email, m.name, SUM(p.price) AS tot_spending
+FROM departments AS d
+INNER JOIN sales AS s
+ON departments.id = sales.department_id
+INNER JOIN products AS p
+ON products.id = sales.product_id
+INNER JOIN members as m
+ON  m.id = s.member_id
+WHERE department_id  IN (SELECT s.department_id
+						FROM sales AS s
+                        INNER JOIN products as p
+                        ON s.product_id = p.id
+                        GROUP BY s.department_id
+                        HAVING SUM(p.price) > 10000)
+GROUP BY member_id, m.email, m.name
+HAVING tot_spending > 1000;
+WITH departments_10k AS (
+						SELECT s.department_id
+						FROM sales AS s
+                        INNER JOIN products as p
+                        ON s.product_id = p.id
+                        GROUP BY s.department_id
+                        HAVING SUM(p.price) > 10000)
+SELECT m.member_id, m.email, m.name, SUM(p.price) AS tot_spending
+FROM departments AS d
+INNER JOIN sales AS s
+ON departments.id = sales.department_id
+INNER JOIN products AS p
+ON products.id = sales.product_id
+INNER JOIN members as m
+ON  m.id = s.member_id
+WHERE department_id  IN ( SELECT  department_id
+								FROM departments_10k)
+GROUP BY member_id, m.email, m.name
+HAVING tot_spending > 1000;
 
 
 
