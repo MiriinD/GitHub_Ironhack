a6e6ced6275a80e9e932268cc333c895bb25da25
MiriinD <Miirigoch@gmail.com>
2024-02-20 18:06:55 +0100

    update

---
+++ Week 6_Musiiic/config.py
@@ -0,0 +1,9 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2759
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 client_id = \'93d1c052f3df5f4aaa8d5f52f38b5009cc\'94\
+client_secret = \'93a1e776acef4c4b2381394b4648a0e7e8\'94}
